69459395bcfb64d8dcaef833563d236b33fca60f
imtuda.dev <110141877+tuda4@users.noreply.github.com>
2023-04-17 23:01:05 +0700

    product collection (#8)

---
+++ src/models/product.model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const {model, Schema} = require('mongoose');
+
+const DOCUMENT_NAME = 'Product'
+const COLLECTION_NAME = 'Products'
+
+const productionSchema = new Schema({
+    productName: {type: String, required: true},
+    productPrice: {type: Number, required: true},
+    productThumbnail: {type: String, required: true},
+    productDescription: {type: String},
+    productQuantity: {type: Number, required: true},
+    productType: {type: String, required: true, enum: ['Perfume', 'Cosmetics']},
+    productShop: {type: Schema.Types.ObjectId, ref: 'Shop'},
+    productAttribute: {type: Schema.Types.Mixed, required: true}
+}, {
+    collection: COLLECTION_NAME,
+    timestamps: true
+})
+
+// define the production type Perfume
+
+const perfumeSchema = new Schema({
+    brand: {type: String, required: true},
+    volume: {type: Number, required: true},
+}, {
+    collection: 'Perfumes',
+    timestamps: true
+})
+
+const cosmeticsSchema = new Schema({
+    brand: {type: String, required: true},
+    model: {type: String, required: true}
+}, {
+    collection: 'Cosmetics',
+    timestamps: true
+})
+
+module.exports =  {
+    product: model(DOCUMENT_NAME, productionSchema),
+    perfume: model('Perfume', perfumeSchema),
+    cosmeticsSchema: model('Cosmetic', cosmeticsSchema)
+}
+
+
